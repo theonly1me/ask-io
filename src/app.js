5e22c5ec62f9a04a537a39a02b0d9ebe86e0e300
theonly1me <pulavarthi.preetham@gmail.com>
2021-01-06 04:38:09 +0530

    Google oAuth in progress

--- src/app.js
+++ src/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+//CID
+//291281965874-gd0tr6ktm694ef95g0gj7t6sfd7rjjb1.apps.googleusercontent.com
+//CSecret
+//apG5k992dGfrphCElPGe4kHw
 
 const app = express();
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+// const strategy = new GoogleStrategy();
+// passport.use(strategy);
 
 //JSON body parser middleware
 app.use(express.json());
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config({ path: `${__dirname}/config.env` });
 const app = require(`./app`);
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}...`);
