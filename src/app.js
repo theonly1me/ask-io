99af73189858ed94687a53ee0ef9a92fa1b69fbb
theonly1me <pulavarthi.preetham@gmail.com>
2021-01-06 04:40:33 +0530

    Removed sensitive information

--- src/app.js
+++ src/app.js
@@ -2,10 +2,6 @@ const express = require('express');
 const morgan = require('morgan');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
-//CID
-//291281965874-gd0tr6ktm694ef95g0gj7t6sfd7rjjb1.apps.googleusercontent.com
-//CSecret
-//apG5k992dGfrphCElPGe4kHw
 
 const app = express();
 
