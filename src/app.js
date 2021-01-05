e72ce9e58d295ea7db2be8fa75fbd64991a2f4ef
theonly1me <pulavarthi.preetham@gmail.com>
2021-01-06 04:19:09 +0530

    Started Google Oauth2

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
-if (!process.env.NODE_ENV) app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 //JSON body parser middleware
 app.use(express.json());
