bf9574f958f256903d1e2046c792a298baf61ecc
theonly1me <pulavarthi.preetham@gmail.com>
2021-01-20 04:41:04 +0530

    more css fixes (Material UI)

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import { FETCH_USER, LOGOUT } from '../actions/types';
 
-const authReducer = (state = {}, action) => {
+const authReducer = (state = null, action) => {
   switch (action.type) {
     case FETCH_USER:
       return action.payload ? { user: action.payload } : false;
